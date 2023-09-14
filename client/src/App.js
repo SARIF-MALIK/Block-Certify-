d54ed4e9a1ea0310a66d24f9eae187ebf748b95a
SARIF <sarif.malik@hotmail.com>
2023-09-14 19:12:30 +0530

    home pg

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Component, useState } from "react";
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+// import {Route, Navigate } from "react-router-dom";
 
 // Nav
 import NavBarInstitute from "./Components/NavBarInstitute";
@@ -14,101 +14,112 @@ import GenerateCert from "./Components/GenerateCert";
 import CertificateDisplay from "./Components/CertificateDisplay";
 import ViewCert from "./Components/ViewCert";
 
-const DynamicLayoutRoute = (props) => {
-  const { component: RoutedComponent, layout, ...rest } = props;
+// const DynamicLayoutRoute = (props) => {
+//   const { component: RoutedComponent, layout, ...rest } = props;
 
-  // render actual Route from react-router
-  const actualRouteComponent = (
-    <Route {...rest} render={(props) => <RoutedComponent {...props} />} />
-  );
+//   // render actual Route from react-router
+//   const actualRouteComponent = (
+//     <Route {...rest} render={(props) => <RoutedComponent {...props} />} />
+//   );
 
-  // depends on the layout, you can wrap Route component in different layouts
-  switch (layout) {
-    case "INSTITUTE": {
-      return (
-        <div>
-          <NavBarInstitute />
-          {actualRouteComponent}
-        </div>
-      );
-    }
-    case "SYSADMIN": {
-      return (
-        <div>
-          <NavBarSysAdmin />
-          {actualRouteComponent}
-        </div>
-      );
-    }
-    default: {
-      return (
-        <div>
-          <NavBarLanding />
-          {actualRouteComponent}
-        </div>
-      );
-    }
-  }
-};
+//   // depends on the layout, you can wrap Route component in different layouts
+//   switch (layout) {
+//     case "INSTITUTE": {
+//       return (
+//         <div>
+//           <NavBarInstitute />
+//           {actualRouteComponent}
+//         </div>
+//       );
+//     }
+//     case "SYSADMIN": {
+//       return (
+//         <div>
+//           <NavBarSysAdmin />
+//           {actualRouteComponent}
+//         </div>
+//       );
+//     }
+//     default: {
+//       return (
+//         <div>
+//           <NavBarLanding />
+//           {actualRouteComponent}
+//         </div>
+//       );
+//     }
+//   }
+// };
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App" style={{ backgroundColor: "#fafafa" }}>
-        <Routes>
-          <DynamicLayoutRoute
-            exact
-            path="/"
-            component={Home}
-            layout="LANDING"
-          />
-          <DynamicLayoutRoute
-            exact
-            path="/admin"
-            component={Admin}
-            layout="SYSADMIN"
-          />
-          <DynamicLayoutRoute
-            exact
-            path="/institute"
-            component={GenerateCert}
-            layout="INSTITUTE"
-          />
-          <DynamicLayoutRoute
-            path="/certificate/:id"
-            component={CertificateDisplay}
-          />
-          <DynamicLayoutRoute path="/view" element={ViewCert} />
-          <DynamicLayoutRoute path="*">
-            <Navigate to="/" />
-          </DynamicLayoutRoute>
-        </Routes>
-      </div>
-    );
-  }
-}
-
-
-
-
-export default App;
-
-// import React from 'react'
-// import Test from "./Components/Test";
-
-// const App = props => {
-//   return (
-//     <div className="App">
-//       <Routes>
-//         <Route
-//             exact path="/"
-//             Component={Home}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App" style={{ backgroundColor: "#fafafa" }}>
+//         <Routes>
+//           <DynamicLayoutRoute
+//             exact
+//             path="/"
+//             component={Home}
 //             layout="LANDING"
 //           />
-
-//     </Routes>
-//     </div>
-//   )
+//           <DynamicLayoutRoute
+//             exact
+//             path="/admin"
+//             component={Admin}
+//             layout="SYSADMIN"
+//           />
+//           <DynamicLayoutRoute
+//             exact
+//             path="/institute"
+//             component={GenerateCert}
+//             layout="INSTITUTE"
+//           />
+//           <DynamicLayoutRoute
+//             path="/certificate/:id"
+//             component={CertificateDisplay}
+//           />
+//           <DynamicLayoutRoute path="/view" element={ViewCert} />
+//           <DynamicLayoutRoute path="*">
+//             <Navigate to="/" />
+//           </DynamicLayoutRoute>
+//         </Routes>
+//       </div>
+//     );
+//   }
 // }
 
-// export default App
+
+import {
+  Route,
+  Routes,
+  Link,
+  Outlet,
+  Navigate,
+} from 'react-router-dom';
+
+
+const App = () => {
+  return (
+      <div className="App" style={{ backgroundColor: "#fafafa" }}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>
+              <Link to="/institute">Institute</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+        </Routes>
+      </div>
+  );
+};
+
+
+export default App; 
